92fa2b6bbb7b65bbf4f6aedf76bd322556a16378
Dain Blodorn Kim <dain@db13.us>
2022-06-14 15:13:31 -0700

    no test page for now

--- pages/test/fixtures.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next'
-import { PageWrapper } from 'components/PageWrapper'
-
-/* @ts-ignore */
-const Fixtures: NextPage = () => {
-  return <PageWrapper direction="column"></PageWrapper>
-}
-
-export default Fixtures
