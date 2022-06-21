237309e864c1be1845cb1ef7ccfc6779ed7a75ff
Dain Blodorn Kim <dain@db13.us>
2022-06-21 12:33:14 -0700

    added seo to collections index page

---
+++ constants/copy.ts
@@ -0,0 +1,7 @@
+/**
+ * Colections Index Page
+ */
+
+export const COLLECTIONS_INDEX_HEADLINE = 'Rankings'
+export const COLLECTIONS_INDEX_COPY =
+  'Nouns are an experimental attempt to improve the formation of on-chain avatar communities. While projects such as Cryptopunks have attempted to bootstrap digital community and identity.'
--- pages/collections/index.tsx
+++ pages/collections/index.tsx
@@ -1,15 +1,14 @@
 import { PageWrapper } from 'components/PageWrapper'
-import { Heading } from '@zoralabs/zord'
+import { COLLECTIONS_INDEX_HEADLINE, COLLECTIONS_INDEX_COPY } from 'constants/copy'
 import { CollectionRanking } from 'compositions/CollectionRanking'
 import { PageHeader } from 'components/PageHeader'
+import { Seo } from 'components/Seo'
 
 const Collections = () => {
   return (
     <PageWrapper p="x4" direction="column" gap="x4">
-      <PageHeader
-        headline="Ranking"
-        copy="Nouns are an experimental attempt to improve the formation of on-chain avatar communities. While projects such as Cryptopunks have attempted to bootstrap digital community and identity."
-      />
+      <Seo title={COLLECTIONS_INDEX_HEADLINE} description={COLLECTIONS_INDEX_COPY} />
+      <PageHeader headline={COLLECTIONS_INDEX_HEADLINE} copy={COLLECTIONS_INDEX_COPY} />
       <CollectionRanking />
     </PageWrapper>
   )
