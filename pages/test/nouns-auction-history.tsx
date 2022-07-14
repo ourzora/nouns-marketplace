775710072d66f899ddf41d49d0a3b02b1712aff0
Dain Blodorn Kim <dain@db13.us>
2022-07-13 18:45:22 -0700

    remove deleted component import

--- pages/test/nouns-auction-history.tsx
+++ pages/test/nouns-auction-history.tsx
@@ -1,10 +1,5 @@
 import { TestPageWrapper } from 'components/utils/TestPageWrapper'
-import { NounAuctionHistory } from '@noun-auction'
 
 export default function NounsAuctionHistory() {
-  return (
-    <TestPageWrapper title="Nouns Auctions">
-      <NounAuctionHistory />
-    </TestPageWrapper>
-  )
+  return <TestPageWrapper title="Nouns Auctions">All nouns auctions</TestPageWrapper>
 }
