f86b697f8283cd0a87c1c858a4a37e9723a74087
nwakaku wisdom <nwakakucc@gmail.com>
2022-08-18 12:58:52 +0100

    some branch

--- compositions/Header/UnstoppableDomainbtn.tsx
+++ compositions/Header/UnstoppableDomainbtn.tsx
@@ -9,7 +9,6 @@ import { useState, useEffect } from 'react'
 const uauth = new UAuth({
   clientID: '1c690135-3701-47c4-9b25-78fa847ca188',
   redirectUri: 'https://nouns-marketplace-eight.vercel.app/',
-  scope: 'openid wallet',
 })
 
 export const UnstoppableDomainbtn = () => {
