f289c6ac58da04b01222db177f6ae085dab0e767
nwakaku wisdom <nwakakucc@gmail.com>
2022-08-18 13:40:53 +0100

    done

--- compositions/Header/UnstoppableDomainbtn.tsx
+++ compositions/Header/UnstoppableDomainbtn.tsx
@@ -8,7 +8,8 @@ import { useState, useEffect } from 'react'
 
 const uauth = new UAuth({
   clientID: '1c690135-3701-47c4-9b25-78fa847ca188',
-  redirectUri: 'https://nouns-marketplace-eight.vercel.app',
+  redirectUri: 'https://sweet-cupcake-eb22aa.netlify.app/',
+  scope: 'openid wallet',
 })
 
 export const UnstoppableDomainbtn = () => {
