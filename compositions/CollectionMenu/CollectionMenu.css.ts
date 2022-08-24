c2516d193d6a090d2cf986fa0e28af30f1bd3d12
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2022-08-23 23:35:05 -0400

    💄 style(CollectionMenu): atomize css

--- compositions/CollectionMenu/CollectionMenu.css.ts
+++ compositions/CollectionMenu/CollectionMenu.css.ts
@@ -1,12 +1,13 @@
 import { style } from '@vanilla-extract/css'
 import { atoms, color, media, space, vars } from '@zoralabs/zord'
+import { MODAL_TAB_LAYER } from '../../constants/layers'
 
 export const collectionTrigger = style([
   {
     gridColumn: '1',
     gridRow: '2',
     height: 42,
-    gridGap: space.x2,
+    gridGap: 'x2',
     '@media': {
       [media.min1024]: {
         gridColumn: '3',
@@ -22,35 +23,28 @@ export const collectionTrigger = style([
   }),
 ])
 
-export const modalWrapper = style({
-  overflowY: 'scroll',
-  msOverflowStyle: 'none',
-  scrollbarWidth: 'none',
-  '::-webkit-scrollbar': {
-    display: 'none',
+export const modalWrapper = style([
+  {
+    msOverflowStyle: 'none',
+    scrollbarWidth: 'none',
+    '::-webkit-scrollbar': {
+      display: 'none',
+    },
+    height: 400,
   },
-  height: 400,
-})
+  atoms({
+    overflowY: 'scroll',
+  }),
+])
 
 export const tabsButton = style([
   {
-    all: 'unset',
-    fontFamily: 'inherit',
+    zIndex: MODAL_TAB_LAYER,
     backgroundColor: `${color.white100}`,
-    padding: '20px',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 20,
-    color: vars.color.text.secondary,
-    cursor: 'pointer',
-    zIndex: 3,
-    width: '100%',
-    paddingBottom: space.x2,
     borderBottom: `2px solid transparent`,
     selectors: {
-      '&:first-child': { borderTopLeftRadius: 40 },
-      '&:last-child': { borderTopRightRadius: 40 },
+      '&:first-child': { borderTopLeftRadius: space.x10 },
+      '&:last-child': { borderTopRightRadius: space.x10 },
       '&:hover': { borderBottom: `2px solid ${color.black30}` },
       '&[data-state="active"]': {
         color: vars.color.text.primary,
@@ -58,16 +52,30 @@ export const tabsButton = style([
       },
     },
   },
+  atoms({
+    borderColor: 'transparent',
+    color: 'tertiary',
+    paddingBottom: 'x2',
+    fontSize: '20px',
+    padding: 'x5',
+    width: '100%',
+    cursor: 'pointer',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }),
 ])
 
 export const tabsList = style([
   {
+    borderBottom: `2px solid ${color.black10}`,
+    zIndex: MODAL_TAB_LAYER,
+  },
+  atoms({
     display: 'flex',
     position: 'absolute',
     width: '100%',
-    borderBottom: `2px solid ${color.black10}`,
-    zIndex: 4,
-  },
+  }),
 ])
 
 export const label = style({
