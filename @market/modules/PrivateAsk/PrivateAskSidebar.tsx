9ddbda535e06cac81d077fef0446803b06ab90f7
BJ Vicks <bj@bjvicks.com>
2022-08-30 15:05:40 -0400

    disable imports in sidebar

--- @market/modules/PrivateAsk/PrivateAskSidebar.tsx
+++ @market/modules/PrivateAsk/PrivateAskSidebar.tsx
@@ -1,9 +1,9 @@
 import { PrivateAskStateProvider } from '@market/modules/PrivateAsk/'
 import { NFTObject } from '@zoralabs/nft-hooks'
 import { Stack, StackProps } from '@zoralabs/zord'
-import { NounsGlasses } from 'components'
+// import { NounsGlasses } from 'components'
 import React from 'react'
-import { PrivateAskModal } from './PrivateAskModal'
+// import { PrivateAskModal } from './PrivateAskModal'
 
 interface PrivateAskSidebarProps extends StackProps {
   nft: NFTObject
@@ -13,6 +13,7 @@ export function PrivateAskSidebar({ nft, ...props }: PrivateAskSidebarProps) {
   return (
     <Stack {...props}>
       <PrivateAskStateProvider>
+        <p>test</p>
         {/* <PrivateAskModal nft={nft} header={<NounsGlasses w="x13" mb="x4" mt="x1" />} /> */}
       </PrivateAskStateProvider>
     </Stack>
