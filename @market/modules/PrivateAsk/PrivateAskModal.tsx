7ad9caea135d4eed3166a4118bfaa3a24a51afdf
BJ Vicks <bj@bjvicks.com>
2022-08-30 15:52:33 -0400

    import directly from PrivateAskStateProvider

--- @market/modules/PrivateAsk/PrivateAskModal.tsx
+++ @market/modules/PrivateAsk/PrivateAskModal.tsx
@@ -15,7 +15,7 @@ import {
   FILLASK,
   FILLASK_SUCCESS,
   usePrivateAskStateContext,
-} from '@market/modules/PrivateAsk/providers/'
+} from '@market/modules/PrivateAsk/providers/PrivateAskStateProvider'
 import { PrivateAskApproveModule } from './PrivateAskApproveModule'
 import { PrivateAskApproveTransferHelper } from './PrivateAskApproveTransferHelper'
 import { PrivateAskCreate } from './create/PrivateAskCreate'
@@ -28,7 +28,8 @@ import { PrivateAskFillAsk } from './fill/PrivateAskFillAsk'
 import { PrivateAskFillAskSuccess } from './fill/PrivateAskFillAskSuccess'
 import { PrivateAskListForSale } from './PrivateAskListForSale'
 import { PrivateAskTrigger } from './PrivateAskTrigger'
-import { useToggle } from '@shared/hooks'
+// import { useToggle } from '@shared/hooks'
+import { useToggle } from '@shared/hooks/useToggle'
 import { NFTObject } from '@zoralabs/nft-hooks'
 import { Flex, Modal, ModalContent, Stack, StackProps } from '@zoralabs/zord'
 // import { AnimatePresence } from 'framer-motion'
