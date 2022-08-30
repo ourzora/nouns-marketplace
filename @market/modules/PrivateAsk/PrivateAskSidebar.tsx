b88e13cc0894eb86e647d82a94db9289378fe246
BJ Vicks <bj@bjvicks.com>
2022-08-30 15:37:49 -0400

    direct import of PrivateAskStateProvider

--- @market/modules/PrivateAsk/PrivateAskSidebar.tsx
+++ @market/modules/PrivateAsk/PrivateAskSidebar.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { PrivateAskStateProvider } from './providers'
 import { NFTObject } from '@zoralabs/nft-hooks'
 import { Stack, StackProps } from '@zoralabs/zord'
 import { NounsGlasses } from 'components'
 import { PrivateAskModal } from './PrivateAskModal'
+import { PrivateAskStateProvider } from './providers/PrivateAskStateProvider'
 
 interface PrivateAskSidebarProps extends StackProps {
   nft: NFTObject
@@ -13,7 +13,6 @@ export function PrivateAskSidebar({ nft, ...props }: PrivateAskSidebarProps) {
   return (
     <Stack {...props}>
       <PrivateAskStateProvider>
-        {/* <p>test</p> */}
         <PrivateAskModal nft={nft} header={<NounsGlasses w="x13" mb="x4" mt="x1" />} />
       </PrivateAskStateProvider>
     </Stack>
