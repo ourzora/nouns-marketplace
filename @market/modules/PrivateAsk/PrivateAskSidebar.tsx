00fcd28ca84c2f89e1388dfc73394417462f83d4
BJ Vicks <bj@bjvicks.com>
2022-08-30 15:34:10 -0400

    test re-enabling of asks sidebar imports

--- @market/modules/PrivateAsk/PrivateAskSidebar.tsx
+++ @market/modules/PrivateAsk/PrivateAskSidebar.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-// import { PrivateAskStateProvider } from './providers'
+import { PrivateAskStateProvider } from './providers'
 import { NFTObject } from '@zoralabs/nft-hooks'
 import { Stack, StackProps } from '@zoralabs/zord'
-// import { NounsGlasses } from 'components'
-// import { PrivateAskModal } from './PrivateAskModal'
+import { NounsGlasses } from 'components'
+import { PrivateAskModal } from './PrivateAskModal'
 
 interface PrivateAskSidebarProps extends StackProps {
   nft: NFTObject
@@ -12,10 +12,10 @@ interface PrivateAskSidebarProps extends StackProps {
 export function PrivateAskSidebar({ nft, ...props }: PrivateAskSidebarProps) {
   return (
     <Stack {...props}>
-      {/* <PrivateAskStateProvider> */}
-      <p>test</p>
-      {/* <PrivateAskModal nft={nft} header={<NounsGlasses w="x13" mb="x4" mt="x1" />} /> */}
-      {/* </PrivateAskStateProvider> */}
+      <PrivateAskStateProvider>
+        {/* <p>test</p> */}
+        <PrivateAskModal nft={nft} header={<NounsGlasses w="x13" mb="x4" mt="x1" />} />
+      </PrivateAskStateProvider>
     </Stack>
   )
 }
