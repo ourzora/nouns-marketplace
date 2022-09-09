3eabc4394c360ee0fe78570ff8d74804081f09b0
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2022-09-09 09:35:08 -0400

    🚸 feat(FilterPropertyAccordion): conditional render search

--- @filter/FilterPropertyAccordion.tsx
+++ @filter/FilterPropertyAccordion.tsx
@@ -3,22 +3,27 @@ import React, { useState } from 'react'
 import { attributeSelectWrapper } from './CollectionsFilter.css'
 import { FilterPropertySelect } from './FilterPropertySelect'
 
+// The minimum amount of properties to display an search input.
+const PROPERTY_MINIMUM = 7
+
 const FilterPropertyAccordion = ({ property }: any) => {
   const [query, setQuery] = useState<string>('')
   return (
     <>
       <Accordion label={property.traitType ? property.traitType : ''}>
-        <Box py="x2" px="x1">
-          <InputField
-            icon="Search"
-            name="search"
-            placeholder={`Search ${property.traitType.toLowerCase()}...`}
-            value={query}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setQuery(e.target.value)
-            }
-          />
-        </Box>
+        {property.valueMetrics.length >= PROPERTY_MINIMUM ? (
+          <Box py="x2" px="x1">
+            <InputField
+              icon="Search"
+              name="search"
+              placeholder={`Search ${property.traitType.toLowerCase()}...`}
+              value={query}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setQuery(e.target.value)
+              }
+            />
+          </Box>
+        ) : null}
         <Stack gap="x2" className={attributeSelectWrapper} mb="x4" px="x1">
           {property.valueMetrics
             .filter(
