87dcb0a5e60990c0ca64e43a2aa3ccd00dcddf1c
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2022-09-09 09:27:26 -0400

    💄 style(FilterPropertySelect): lighten metrics to tertiary
    clean up additional unnecessary components

--- @filter/FilterPropertySelect.tsx
+++ @filter/FilterPropertySelect.tsx
@@ -1,7 +1,15 @@
 import { filterPropertySelect } from './CollectionsFilter.css'
 import { useCollectionFilters } from './providers/CollectionFilterProvider'
 import { CollectionAttributeValue } from '@zoralabs/zdk/dist/queries/queries-sdk'
-import { Box, Button, Checkbox, Flex, Paragraph, Stack, fontWeight } from '@zoralabs/zord'
+import {
+  Text,
+  Button,
+  Checkbox,
+  Flex,
+  Paragraph,
+  Stack,
+  fontWeight,
+} from '@zoralabs/zord'
 import { useCallback, useMemo } from 'react'
 
 export function FilterPropertySelect({
@@ -29,31 +37,20 @@ export function FilterPropertySelect({
 
   return (
     <Button variant="unset" w="100%" className={filterPropertySelect} onClick={setValue}>
-      <Box
-        className={filterPropertySelect}
-        direction="row"
-        justify="stretch"
-        align="center"
-      >
-        <Flex>
-          <Checkbox
-            name={valueMetric.value}
-            id={valueMetric.value}
-            checked={isSelected}
-          />
-          <Paragraph ml="x2" size="sm">
-            {valueMetric.value}
-          </Paragraph>
-        </Flex>
-        <Stack gap="x0">
-          <Paragraph size="sm" align="right" fontWeight="label">
-            {valueMetric.count}
-          </Paragraph>
-          <Paragraph size="xs" fontWeight="label">
-            ({valueMetric.percent}%)
-          </Paragraph>
-        </Stack>
-      </Box>
+      <Flex>
+        <Checkbox name={valueMetric.value} id={valueMetric.value} checked={isSelected} />
+        <Paragraph ml="x2" size="sm">
+          {valueMetric.value}
+        </Paragraph>
+      </Flex>
+      <Stack gap="x0">
+        <Text variant="label-sm" color="tertiary" align="right">
+          {valueMetric.count}
+        </Text>
+        <Text variant="label-xs" color="tertiary">
+          ({valueMetric.percent}%)
+        </Text>
+      </Stack>
     </Button>
   )
 }
