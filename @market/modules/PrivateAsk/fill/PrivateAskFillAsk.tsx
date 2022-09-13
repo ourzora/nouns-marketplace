5266a0276a8e999a892cacb4a274f00ce027abff
BJ Vicks <bj@bjvicks.com>
2022-09-13 15:39:20 -0400

    re-add CollectionThumbnail

--- @market/modules/PrivateAsk/fill/PrivateAskFillAsk.tsx
+++ @market/modules/PrivateAsk/fill/PrivateAskFillAsk.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useMemo } from 'react'
 import { TransactionSubmitButton } from '@market/components/TransactionSubmitButton'
 // import { PriceWithLabel } from '@shared/components/PriceWithLabel'
 import { CommonPrivateAskComponentProps } from '../PrivateAskFlow'
-// import { CollectionThumbnail } from '@media/CollectionThumbnail'
+import { CollectionThumbnail } from '@media/CollectionThumbnail'
 import { useRelevantMarket } from '@market/hooks/useRelevantMarket'
 import { useAskHelper } from '@market/hooks/useAskHelper'
 import { usePrivateAskTransaction } from '../hooks/usePrivateAskTransaction'
@@ -34,11 +34,11 @@ export function PrivateAskFillAsk({ onNext, ...props }: PrivateAskFillAskProps) 
   return (
     <Stack gap="x6">
       <Flex w="100%" justify="space-between" textAlign="right">
-        {/*
         <CollectionThumbnail
           collectionAddress={nft?.contract.address}
           tokenId={nft?.tokenId}
         />
+        {/*
         {displayAskAmount && (
           <PriceWithLabel
             label="Private Listing"
