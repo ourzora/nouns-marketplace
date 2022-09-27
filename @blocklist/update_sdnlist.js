d7189b75e8af325b3a82a77fc945d270882d4443
Oleg Akbarov <oleg.akbarov@pm.me>
2022-09-27 16:54:05 -0700

    blocklist script rewritten in js

---
+++ @blocklist/update_sdnlist.js
@@ -0,0 +1,24 @@
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+const writeFile = util.promisify(require('fs').writeFile)
+
+async function run() {
+  try {
+    const curlString = 'curl -s https://www.treasury.gov/ofac/downloads/sdnlist.txt'
+    const fetchData = exec(curlString, { maxBuffer: 1024 * 1024 * 1024 })
+    const { stdout, stderr } = await fetchData
+    if (stderr) {
+      console.error(`error: ${stderr}`)
+      return
+    }
+
+    const res = stdout.toString().match(/0x[a-fA-F0-9]{40}/g)
+    // FIXME: isaac adress for testing
+    const unique = [...new Set(res), '0x8F474C2beae9D20Ae0F19c80266bfA04023830CB']
+    writeFile('./@blocklist/sdnlist.json', JSON.stringify(unique, null, 2))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+run()
