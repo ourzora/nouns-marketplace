cdf5d91e8187b25b31db736c0a25c90291a2d7bd
BJ Vicks <bj@bjvicks.com>
2022-09-29 16:01:06 -0400

    Add copy/paste option to PrivateCreateAskSuccess

--- @market/modules/PrivateAsk/create/PrivateAskCreateSuccess.tsx
+++ @market/modules/PrivateAsk/create/PrivateAskCreateSuccess.tsx
@@ -1,16 +1,37 @@
 import { Button, Stack } from '@zoralabs/zord'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { HeadingDescription } from '../HeadingDescription'
 import { CommonPrivateAskComponentProps } from '../PrivateAskFlow'
 import { SuccessCheckmark } from '@market/components/SuccessCheckmark'
+import { useCopyToClipboard } from '@shared'
+import { useToast } from '@toast'
+import { ToastStatus, ToastVariant } from '@toast/toastReducer'
 
 interface PrivateAskCreateSuccessProps extends CommonPrivateAskComponentProps {}
 
 export function PrivateAskCreateSuccess({
-  nft,
+  nft: nftObj,
   onNext,
   ...props
 }: PrivateAskCreateSuccessProps) {
+  const { nft } = nftObj
+  const askURL = `https://noun.market/collections/${nft?.contract.address}/${nft?.tokenId}`
+  const [_, copied, copy] = useCopyToClipboard(askURL)
+  const { toastDispatch } = useToast()
+
+  useEffect(() => {
+    if (copied) {
+      toastDispatch({
+        type: ToastStatus.REPLACE,
+        item: {
+          duration: 3500,
+          description: 'Copied to clipboard.',
+          variant: ToastVariant.SUCCESS,
+        },
+      })
+    }
+  }, [copied, toastDispatch])
+
   return (
     <Stack gap="x8" {...props}>
       <SuccessCheckmark />
@@ -18,9 +39,14 @@ export function PrivateAskCreateSuccess({
         heading="Private Listing Created"
         description="This listing is now available to the buyer."
       />
-      <Button onClick={onNext} borderRadius="curved">
-        Done
-      </Button>
+      <Stack gap="x2">
+        <Button variant="secondary" onClick={copy} borderRadius="curved">
+          Copy Listing URL
+        </Button>
+        <Button onClick={onNext} borderRadius="curved">
+          Done
+        </Button>
+      </Stack>
     </Stack>
   )
 }
