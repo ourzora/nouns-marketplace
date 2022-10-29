c5fe0c4d2debab4cdab65579623e8685d9b4ffd7
BJ Vicks <bj@bjvicks.com>
2022-10-28 20:47:55 -0400

    remove stray error comment

--- @shared/utils/formatContractError.ts
+++ @shared/utils/formatContractError.ts
@@ -9,8 +9,6 @@ export interface TransactionError extends Error {
   transaction: string
 }
 
-// ERROR: cannot estimate gas; transaction may fail or may require manual gas limit [ See: https://links.ethers.org/v5-errors-UNPREDICTABLE_GAS_LIMIT ] (reason="execution reverted", method="estimateGas", transaction={"from":"0x9FefE8a875E7a9b0574751E191a2AF205828dEA4","to":"0x6170B3C3A54C3d8c854934cBC314eD479b2B29A3","data":"0xc4f0ee36000000000000000000000000decc60000ba66700a009b8f9f7d82676b5cfa88a0000000000000000000000000000000000000000000000000000000000002456000000000000000000000000000000000000000000000000000000e8d4a51000","accessList":null}, error={"code":-32000,"message":"execution reverted"}, code=UNPREDICTABLE_GAS_LIMIT, version=providers/5.7.1)
-
 export function formatContractError(error: TransactionError | Error) {
   if ('transaction' in error) {
     switch (error.reason) {
